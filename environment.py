51e7faa60217cb7ac4c65465a0e80d965a3ee717
49ball <49ball@naver.com>
2024-07-16 14:46:18 +0900

    JT-param

--- environment.py
+++ environment.py
@@ -16,6 +16,8 @@ class MapWithObstacles:
         self.obstacles = []
         if seed is not None:
             np.random.seed(seed)  # 재현 가능한 결과를 위해 시드 설정
+        else:
+            np.random.seed()
         self.generate_obstacles()
 
     def generate_obstacles(self):
@@ -47,15 +49,16 @@ class MapWithObstacles:
         states = torch.tensor(states, dtype=torch.float32, device='cuda')
         x, y, v = states[:, 0], states[:, 1], states[:, 3]
         goal = torch.tensor([100, 100], dtype=torch.float32, device='cuda')
+        max_reward= 1.42
         
         
         k_obs = 50.0
-        sigma_x = 3.0  # 수렴 계수
-        sigma_y = 3.0  # 수렴 계수
+        sigma_x = 4.0  # 수렴 계수
+        sigma_y = 4.0  # 수렴 계수
         
         # 목표 지점관련 리워드
         distance_to_goal = torch.sqrt((x - goal[0])**2 + (y - goal[1])**2)
-        reward = -distance_to_goal
+        reward = -distance_to_goal*0.01
         
         # 장애물간의 거리를 계산한 리워드
         obstacles = torch.tensor(self.obstacles, dtype=torch.float32, device='cuda')
@@ -63,21 +66,25 @@ class MapWithObstacles:
         obs_y = obstacles[:, 1].view(1, -1)
         distance_to_obstacles = ((x.view(-1, 1) - obs_x)**2 + (y.view(-1, 1) - obs_y)**2).clone().detach()
         Cost = torch.exp(-(((x.view(-1, 1) - obs_x)**2 / sigma_x**2) + ((y.view(-1, 1) - obs_y)**2 / sigma_y**2)))
-        reward -= k_obs * torch.sum(Cost, dim=1)
+        reward -= k_obs * torch.sum(Cost, dim=1)*0.01
         done = False     
 
         #차량 속도와 관련된 리워드
-        speed_reward = torch.clamp(v / 3.0, 0.0, 1.0) #속도 리워드 정규화(0에서 3까지)
+       # speed_reward = torch.clamp(v / 3.0, 0.0, 1.0) #속도 리워드 정규화(0에서3)
+        speed_reward= 0.0
+        if speed_reward<0.0:
+            speed_reward=-1
         reward += speed_reward
 
         if torch.any(distance_to_obstacles < 3.5, dim=1): #차가 장애물과의 거리가 너무 가까울땐 중단
-            done = True
+            done =True
 
-        elif distance_to_goal <=0.5: #차가 목표지점에 도착했을때
-            reward = torch.tensor(200.0, dtype=torch.float32, device='cuda')
+        if distance_to_goal <=0.5: #차가 목표지점에 도착했을때
+            reward = torch.tensor(10.0, dtype=torch.float32, device='cuda')
             done = True
 
         elif torch.any(x <= 0) or torch.any(x >= 100) or torch.any(y <= 0) or torch.any(y >= 100): #차가 경계선 밖을 나갈때
+            reward = torch.tensor(-5.0, dtype=torch.float32, device='cuda')
             done = True
         
         return reward.cpu().numpy().item(), done
@@ -126,7 +133,7 @@ def make_env(cfg):
     return env
 
 # map_with_obstacles = MapWithObstacles()
-# state = np.array([8, 8, 0, 0])
+# state = np.array([0, 0, 0, 0])
 # reward, done = map_with_obstacles.calculate_reward(state)
 # print(f"Reward for state {state} : {reward}, Done: {done}")
 
